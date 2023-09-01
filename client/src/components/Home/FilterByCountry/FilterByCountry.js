232e6bc39e07b877024a657e1553fdf7671b128e
Christian <hernandezchristian94@gmail.com>
2023-09-01 16:44:36 -0700

    started with the country category and icons, added a state where the country name gets stored when you click on the country icon

--- client/src/components/Home/FilterByCountry/FilterByCountry.js
+++ client/src/components/Home/FilterByCountry/FilterByCountry.js
@@ -2,16 +2,19 @@ import React from 'react';
 import './styles.css';
 
 const countries = [
-    { name: 'USA', icon: 'united-states.png' },
-    { name: 'Canada', icon: 'canada.png' },
-    { name: 'Mexico', icon: 'mexico.png' },
-    { name: 'UK', icon: 'united-kingdom.png' },
-    { name: 'Japan', icon: 'japan.png' },
-    { name: 'South Korea', icon: 'south-korea.png' },
-    { name: 'Singapore', icon: 'singapore.png' },
-    { name: 'Portugal', icon: 'portugal.png' },
-    { name: 'Spain', icon: 'spain.png' },
-    { name: 'Australia', icon: 'australia.png' },
+    { name: 'USA', countryCode: "US", icon: 'united-states.png' },
+    { name: 'Canada', countryCode: "CA", icon: 'canada.png' },
+    { name: 'Mexico', countryCode: "MX", icon: 'mexico.png' },
+    { name: 'UK', countryCode: "UK", icon: 'united-kingdom.png' },
+    { name: 'France', countryCode: "FR", icon: 'france.png' },
+    { name: 'Germany', countryCode: "DE", icon: 'germany.png' },
+    { name: 'Japan', countryCode: "JP", icon: 'japan.png' },
+    { name: 'South Korea', countryCode: "KR", icon: 'south-korea.png' },
+    { name: 'Singapore', countryCode: "SG", icon: 'singapore.png' },
+    { name: 'Portugal', countryCode: "PT", icon: 'portugal.png' },
+    { name: 'Spain', countryCode: "ES", icon: 'spain.png' },
+    { name: 'South Africa', countryCode: "ZA", icon: 'south-africa.png' },
+    { name: 'Australia', countryCode: "AU", icon: 'australia.png' },
 ];
 
 function FilterByCountry({ setCountryQuery }) {
@@ -25,7 +28,7 @@ function FilterByCountry({ setCountryQuery }) {
             <ul>
                 {countries.map((country) => (
                     <li key={country.name}>
-                        <button onClick={() => handleCountryClick(country.name)}>
+                        <button onClick={() => handleCountryClick(country.countryCode)}>
                             <img src={require(`../../../assets/${country.icon}`)} alt={country.name} />
                             {country.name}
                         </button>
