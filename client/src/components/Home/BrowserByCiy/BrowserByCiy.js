6cf843adb6ec5592385d4ae57ff24f25496c1e83
Christian <hernandezchristian94@gmail.com>
2023-09-05 18:40:10 -0700

    added a popular destination component in the home component

--- client/src/components/Home/BrowserByCiy/BrowserByCiy.js
+++ client/src/components/Home/BrowserByCiy/BrowserByCiy.js
@@ -1,10 +1,7 @@
-import React, { useState } from 'react'
-import { GrFormPrevious } from 'react-icons/gr';
-import { GrFormNext } from 'react-icons/gr';
+import React from 'react'
 import './styles.css'
 
 function BrowserByCity() {
-    const [xAxis, setXAxis] = useState(0)
 
     const cities = [
         { name: "Los Angeles", icon: "los-angeles.png" },
@@ -17,30 +14,13 @@ function BrowserByCity() {
         { name: "Montreal", icon: "montreal.jpg" }
     ]
 
-    const handlePrev = () => {
-        if (xAxis === 0) {
-            setXAxis(0)
-        } else {
-            setXAxis(xAxis - 270)
-        }
-    }
-
-    const handleNext = () => {
-        if (xAxis === 810) {
-            setXAxis(0)
-        } else {
-            setXAxis(xAxis + 270)
-        }
-    }
-
     return (
         <section className='BrowserByCity'>
             <h4>Popular Destinations</h4>
             <div className='city-wrapper'>
-                <button onClick={handlePrev} className='prev'><GrFormPrevious /></button>
                 <ul >
                     {cities.map((city) => (
-                        <li key={city.name} style={{ position: "relative", right: xAxis }}>
+                        <li key={city.name} >
                             <button>
                                 <img src={require(`../../../assets/${city.icon}`)} alt={city.name} />
                                 {city.name}
@@ -48,7 +28,6 @@ function BrowserByCity() {
                         </li>
                     ))}
                 </ul>
-                <button onClick={handleNext} className='next'><GrFormNext /></button>
             </div>
         </section>
     );
